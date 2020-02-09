a15b23f0bd2f59150734337be29920682164c4d1
Gagah Pangeran <gagahpangeran@gmail.com>
2020-02-10 01:20:43 +0700

    add material ui

--- koyomi/src/App.js
+++ koyomi/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Typography component="header" variant="h1">
+          Hello World
+        </Typography>
+      </Container>
+    </React.Fragment>
   );
 }
 
