8ee13848b20e7e4bfe3f220a93398450cab310ff
Gagah Pangeran <gagahpangeran@gmail.com>
2020-02-10 10:23:23 +0700

    remove material ui

---
+++ koyomi/src/app/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Form from "../components/Form";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Form />
+    </React.Fragment>
+  );
+}
+
+export default App;
