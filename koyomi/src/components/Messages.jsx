d5a368b0a7f388bb684f02137ca8fa6d5cec8b8c
Gagah Pangeran <gagahpangeran@gmail.com>
2020-02-13 00:54:31 +0700

    add messages css

--- koyomi/src/components/Messages.jsx
+++ koyomi/src/components/Messages.jsx
@@ -3,28 +3,36 @@ import { useQuery } from "@apollo/react-hooks";
 
 import { GET_MESSAGE } from "../gql/message.graphql";
 
+import "../styles/messages.css";
+
 export default function Messages() {
   const { data, loading, error } = useQuery(GET_MESSAGE, {
     fetchPolicy: "network-only"
   });
 
   if (loading) {
-    return <>Loading...</>;
+    return <div className="kym-box">Loading...</div>;
   }
 
   if (data === undefined || error) {
-    return <>Error</>;
+    return <div className="kym-box">Error, something went wrong.</div>;
+  }
+
+  if (data === []) {
+    return (
+      <div className="kym-box">No message yet. You can be the first one!</div>
+    );
   }
 
   return (
-    <div>
+    <section className="kym-messages">
       {data.message.map(({ id, from, to, message }) => (
-        <ul key={id}>
-          <li>From : {from}</li>
-          <li>To : {to}</li>
-          <li>Message : {message}</li>
-        </ul>
+        <div key={id} className="kym-message">
+          <p>From : {from}</p>
+          <p>To : {to}</p>
+          <p>Message : {message}</p>
+        </div>
       ))}
-    </div>
+    </section>
   );
 }
